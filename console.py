d3de2aa9efcdd94f7198859cab484f3399a5fddc
starkjoy <residualcode6@gmail.com>
2023-06-19 17:54:48 +0000

    Updated do_create function

--- console.py
+++ console.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Console Module """
 
-
 import cmd
 import sys
+from shlex import split
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
