b1c0fd0939166d64549ac04f4f5b0c7bf3fd6d06
starkjoy <residualcode6@gmail.com>
2023-07-10 11:04:45 +0000

    Fixed issues

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,17 +8,18 @@ from datetime import datetime
 
 
 @task
-def do_pack():
+def do_pack(c):
     """ Creates a tar gzipped archive of the directory web_static """
     dt = datetime.utcnow()
     file = "versions/web_static_{}{}{}{}{}{}.tgz".format(
             dt.year,
             dt.month,
+            dt.day,
             dt.hour,
             dt.minute,
             dt.second)
-    if run("test -d versions", warn=True).failed:
-        run("mkdir -p versions")
-    if run("tar -cvzf {} web_static".format(file), warn=True).failed:
+    if c.run("test -d versions", warn=True).failed:
+        c.run("mkdir -p versions")
+    if c.run("tar -cvzf {} web_static".format(file), warn=True).failed:
         return None
     return file
